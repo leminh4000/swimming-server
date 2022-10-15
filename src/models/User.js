f70cb66d9660dae61a13e05e8b8fcd888beea4b4
Le Minh <leminh2000@gmail.com>
2022-10-15 16:10:21 +0700

    new design: signin, question

--- src/models/User.js
+++ src/models/User.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    email: {
+    username: {
         type: String,
         unique: true,
         required: true
+    },
+    email: {
+        type: String,
+        unique: true,
     },
     password: {
         type: String,
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,10 +6,10 @@ const User = mongoose.model('User');
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
-    const { email, password } = req.body;
+    const { username,email, password } = req.body;
 
     try {
-        const user = new User({ email, password });
+        const user = new User({ username, email, password });
         await user.save();
 
         const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
@@ -21,22 +21,23 @@ router.post('/signup', async (req, res) => {
 });
 
 router.post('/signin', async (req, res) => {
-    const { email, password } = req.body;
+    const { username, password } = req.body;
 
-    if (!email || !password) {
-        return res.status(422).send({ error: 'Must provide email and password'});
+    if (!username || !password) {
+        return res.status(422).send({ error: 'Must provide username and password'});
     }
 
-    const user = await User.findOne({email});
+
+    const user = await User.findOne({username});
     if (!user){
-        return res.status(422).send({ error: "Invalid password or email"});
+        return res.status(422).send({ error: "Invalid password or username"});
     }
     try {
         await user.comparePassword(password);
         const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
         res.send({ token });
     } catch (err){
-        return res.status(422).send({error: "Invalid password or email"})
+        return res.status(422).send({error: "Invalid password or username"})
     }
 });
 
