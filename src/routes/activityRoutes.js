7ca938a6b917e7bdb48e2871018daa6cbd41397e
Le Minh <leminh2000@gmail.com>
2022-10-21 19:11:11 +0700

    #new design: activities week, month

--- src/routes/activityRoutes.js
+++ src/routes/activityRoutes.js
@@ -12,25 +12,23 @@ router.use(requireAuth);
 router.get('/activitiesSummary', async (req, res) => {
     // console.log('req.query', req.query);
     const from = new Date(req.query.from);
-    const to = new Date(req.query.to);
     const activities = await Activity.find({
-        userId    : req.user._id,
+        userId   : req.user._id,
         timestamp: {
             $gte: from,
-            $lt : to
         }
     });
     console.log(activities);
-    const sessions=[];
-    const avg_heart_rates =[];
-    const enhanced_avg_speeds=[];
+    const sessions = [];
+    const avg_heart_rates = [];
+    const enhanced_avg_speeds = [];
     const total_caloriesArray = [];
-    const total_distances= [];
-    const total_timer_times=[];
-    const pool_length= activities[0].sessions[0].pool_length;
+    const total_distances = [];
+    const total_timer_times = [];
+    const pool_length = activities[0].sessions[0].pool_length;
 
     for (const activity of activities) {
-        const session=activity.sessions[0];
+        const session = activity.sessions[0];
         avg_heart_rates.push(session.avg_heart_rate);
         enhanced_avg_speeds.push(session.enhanced_avg_speed);
         total_caloriesArray.push(session.total_calories);
@@ -39,13 +37,19 @@ router.get('/activitiesSummary', async (req, res) => {
     }
 
     const avg_heart_rate = avg_heart_rates.reduce((a, b) => a + b, 0) / avg_heart_rates.length + " bpm";
-    const enhanced_avg_speed = Math.floor(((enhanced_avg_speeds.reduce((a, b) => a + b, 0) / enhanced_avg_speeds.length) * pool_length)/60) + "p:" + pool_length + "m";
-    const total_calories=total_caloriesArray.reduce((a, b) => a + b, 0) + " calories";
-    const total_distance=total_distances.reduce((a, b) => a + b, 0)/1000 + " km";
-    const total_timer_time=new Date(total_timer_times.reduce((a, b) => a + b, 0) * 1000).toISOString().substr(11, 5) + " phút";
+    const enhanced_avg_speed = Math.floor(((enhanced_avg_speeds.reduce((a, b) => a + b, 0) / enhanced_avg_speeds.length) * pool_length) / 60) + "p:" + pool_length + "m";
+    const total_calories = total_caloriesArray.reduce((a, b) => a + b, 0) + " calories";
+    const total_distance = total_distances.reduce((a, b) => a + b, 0) / 1000 + " km";
+    const total_timer_time = new Date(total_timer_times.reduce((a, b) => a + b, 0) * 1000).toISOString().substr(11, 5) + " phút";
 
 
-    res.send({avg_heart_rate, enhanced_avg_speed, total_calories, total_distance,total_timer_time});
+    res.send({
+        avg_heart_rate,
+        enhanced_avg_speed,
+        total_calories,
+        total_distance,
+        total_timer_time
+    });
 });
 router.get('/activities', async (req, res) => {
     const activities = await Activity.find({userId: req.user._id}).sort({"timestamp": -1}).limit(4);
@@ -53,78 +57,33 @@ router.get('/activities', async (req, res) => {
     res.send(activities);
 });
 router.get('/activities2', async (req, res) => {
-    console.log("req.query",req.query);
-    const id = req.query.id;
+    console.log("req.query", req.query);
+    console.log("req.user._id", req.user._id);
 
-    Activity.findById(id)
+    if (req.query.id){
+        Activity.findById(req.query.id)
         .then(data => {
-            if (!data)
-                res.status(422).send({message: "Not found Activity with id " + id});
-            else res.send(data);
+            if (!data) res.status(422).send({message: "Not found Activity with id " + req.query.id}); else res.send(data);
         })
         .catch(err => {
             res
                 .status(422)
-                .send({message: "Error retrieving Activity with id=" + id});
+                .send({message: "Error retrieving Activity with id=" + req.query.id});
         });
+    } else if (req.query.from){
+        const from = new Date(req.query.from);
+        console.log("from", from);
+        const activities = await Activity.find({
+            userId   : req.user._id,
+            timestamp: {
+                $gte: from,
+            }
+        });
+        console.log("ctivities.length",activities.length);
+        return res.send(activities);
+    }
+    return res.status(422).send({error: 'Wrong params'});
 });
-
-
-async function updateArchivement(req, activity) {
-    let archivementTotal = await Archivement.findOne({type: "total"});
-    // console.log("archivement", archivementTotal);
-    if (!archivementTotal) {
-        archivementTotal = new Archivement({
-            category: "total",
-            type    : "total",
-            userId  : req.user._id
-        });
-    }
-    archivementTotal.value += activity.sessions[0].total_distance;
-    await archivementTotal.save();
-
-    let archivementLevel = await Archivement.findOne({type: "level"});
-    // console.log("archivementLevel", archivementLevel);
-    if (!archivementLevel) {
-        archivementLevel = new Archivement({
-            category: "medal",
-            type    : "level",
-            value: 0,
-            userId  : req.user._id
-        });
-    }
-
-    switch (true) {
-        case archivementTotal.value >= 150:
-            archivementLevel.value = 4;
-            break;
-        case archivementTotal.value >= 100:
-            archivementLevel.value = 3;
-            break;
-        case archivementTotal.value >= 50:
-            archivementLevel.value = 2;
-            break;
-        case archivementTotal.value >= 20:
-            archivementLevel.value = 1;
-            break;
-    }
-    archivementLevel.save();
-
-    let archivementLongest = await Archivement.findOne({type: "Longest"});
-    // console.log("archivementLongest", archivementLongest);
-    if (!archivementLongest) {
-        archivementLongest = new Archivement({
-            category: "distance",
-            type    : "Longest",
-            value: 0,
-            userId  : req.user._id
-        });
-    }
-    if (archivementLongest.value < activity.sessions[0].total_distance){
-        archivementLongest.value = activity.sessions[0].total_distance;
-    }
-    await archivementLongest.save();
-}
 
 router.post('/activities', async (req, res) => {
     // console.log('req.body', req.body);
@@ -174,6 +133,129 @@ router.put('/activities', async (req, res) => {
             });
         });
 });
+
+
+async function updateSpeedArchivement(req, activity, speedArchivementLength) {
+    //update archivements speed
+
+    let archivementSpeed = await Archivement.findOne({type: `${speedArchivementLength} m`});
+    if (!archivementSpeed) {
+        archivementSpeed = new Archivement({
+            category: "speed",
+            type    : `${speedArchivementLength} m`,
+            value   : 0,
+            userId  : req.user._id
+        });
+    }
+
+
+    if (!(speedArchivementLength % activity.sessions[0].pool_length)) {
+        const totalTimes = [];
+        console.log("pool_length", activity.sessions[0].pool_length);
+        const laps = activity.sessions[0].laps;
+
+        for (const lap of laps) {
+            let continuedLengthSlices = speedArchivementLength / activity.sessions[0].pool_length;
+            console.log("lap.lengths.length",lap.lengths.length);
+            if (lap.lengths.length >= continuedLengthSlices) {
+                for (let offset = 0; offset <= lap.lengths.length - continuedLengthSlices; offset++) {
+                    let total = 0;
+                    for (let segmentIndex = 0; segmentIndex < continuedLengthSlices; segmentIndex++) {
+                        total += lap.lengths[offset + segmentIndex].total_timer_time;
+                    }
+                    totalTimes.push(total);
+                }
+            }
+        }
+        console.log('speedArchivementLength', speedArchivementLength)
+        console.log("totalTimes", totalTimes);
+        console.log("speedArchivementLength", speedArchivementLength);
+        const timeMin = Math.min(...totalTimes);
+        console.log("timeMin", timeMin);
+        console.log("archivementSpeed", archivementSpeed);
+        if (!archivementSpeed.value || timeMin < archivementSpeed.value) archivementSpeed.value = Math.round(timeMin);
+    }
+
+    await archivementSpeed.save();
+}
+
+async function updateLongestArchivement(req, activity) {
+    //update archivements longest
+    let archivementLongest = await Archivement.findOne({type: "Longest"});
+    // console.log("archivementLongest", archivementLongest);
+    if (!archivementLongest) {
+        archivementLongest = new Archivement({
+            category: "distance",
+            type    : "Longest",
+            value   : 0,
+            userId  : req.user._id
+        });
+    }
+    if (archivementLongest.value < activity.sessions[0].total_distance) {
+        archivementLongest.value = activity.sessions[0].total_distance;
+    }
+    await archivementLongest.save();
+}
+
+async function updateLevelArchivement(req, archivementTotal) {
+    //update archivements level
+    let archivementLevel = await Archivement.findOne({type: "level"});
+    // console.log("archivementLevel", archivementLevel);
+    if (!archivementLevel) {
+        archivementLevel = new Archivement({
+            category: "medal",
+            type    : "level",
+            value   : 0,
+            userId  : req.user._id
+        });
+    }
+
+    switch (true) {
+        case archivementTotal.value >= 150:
+            archivementLevel.value = 4;
+            break;
+        case archivementTotal.value >= 100:
+            archivementLevel.value = 3;
+            break;
+        case archivementTotal.value >= 50:
+            archivementLevel.value = 2;
+            break;
+        case archivementTotal.value >= 20:
+            archivementLevel.value = 1;
+            break;
+    }
+    archivementLevel.save();
+}
+
+async function updateTotalArchivement(req, activity) {
+    //update archivements total
+    let archivementTotal = await Archivement.findOne({type: "total"});
+    // console.log("archivement", archivementTotal);
+    if (!archivementTotal) {
+        archivementTotal = new Archivement({
+            category: "total",
+            type    : "total",
+            userId  : req.user._id
+        });
+    }
+    archivementTotal.value += activity.sessions[0].total_distance;
+    await archivementTotal.save();
+    return archivementTotal;
+}
+
+async function updateArchivement(req, activity) {
+    let archivementTotal = await updateTotalArchivement(req, activity);
+
+    await updateLevelArchivement(req, archivementTotal);
+
+    await updateLongestArchivement(req, activity);
+
+    await updateSpeedArchivement(req, activity,100);
+    await updateSpeedArchivement(req, activity,400);
+    await updateSpeedArchivement(req, activity, 750);
+
+
+}
 
 
 module.exports = router;
